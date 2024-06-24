a8e76575a07d7bb2973889b549162ae7ff468d04
Gofer2x <mrgoferplx@gmail.com>
2024-06-25 01:44:44 +0200

    *you can
    +thanks vesly

--- Enumerate/Enumerate_All.bat
+++ Enumerate/Enumerate_All.bat
@@ -1,9 +1,10 @@
+REM got this from some link vesly posted. thanks! putting this here so i can properly credit.
 @echo off
 setlocal enabledelayedexpansion
 set FILE_MATCH=*.txt
 
 for /R "%~dp0" %%F in (%FILE_MATCH%) do (
-  set OUT_FILE=%%~dF%%~pF%%~nF%.enu.event
+  set OUT_FILE=%%~dF%%~pF%%~nF%.event
   echo Assembling "%%~nxF"...
   enumerate.py "%%~nxF" "!OUT_FILE!"
 )

c244652417d278c1649a0170a336cf67d7f4119e
Gofer2x <mrgoferplx@gmail.com>
2024-07-04 23:34:24 +0200

    hardcoded classes stuff, enemy ai, class abilities

---
+++ Events/REDAs Generator.py
@@ -0,0 +1,14 @@
+output = []
+
+maxX = 48
+maxY = 48
+
+output.append("ALIGN 4\n")
+
+for x in range(maxY):
+    for y in range(maxY):
+        output.append("REDA"+str(x)+"R"+str(y)+":\n")
+        output.append("REDA ["+str(x)+","+str(y)+"] 0x0 0x0 0x0 0x0\n")
+
+with open("REDAs.event","w") as write:
+    write.writelines(output)

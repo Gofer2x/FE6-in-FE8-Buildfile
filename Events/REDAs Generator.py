49c4bd200947b59d356f0eb0ad86bb09053f47a5
Gofer2x <mrgoferplx@gmail.com>
2024-07-14 21:40:30 +0200

    lot (haha) of ch2 done

--- Events/REDAs Generator.py
+++ Events/REDAs Generator.py
@@ -7,7 +7,7 @@ output.append("ALIGN 4\n")
 
 for x in range(maxY):
     for y in range(maxY):
-        output.append("REDA"+str(x)+"R"+str(y)+":\n")
+        output.append("REDA_"+str(x)+"_"+str(y)+":\n")
         output.append("REDA ["+str(x)+","+str(y)+"] 0x0 0x0 0x0 0x0\n")
 
 with open("REDAs.event","w") as write:

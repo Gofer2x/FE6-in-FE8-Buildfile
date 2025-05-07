fc4c0f18ee2308ba352f3c540d8d9841dbe09639
Gofer2x <45763255+Gofer2x@users.noreply.github.com>
2025-05-07 21:03:26 +0200

    Update UpdateIDsFromTables.cmd

--- UpdateIDsFromTables.cmd
+++ UpdateIDsFromTables.cmd
@@ -1,6 +1,6 @@
-"Tools/TableUpdater.py" "Tables/NightmareModules/CharacterAndClass/CharacterTable.csv" "Definitions/Generated/CharacterIDs.event" "Character" "--OneIndexed" 
-"Tools/TableUpdater.py" "Tables/NightmareModules/CharacterAndClass/ClassTable.csv" "Definitions/Generated/ClassIDs.event" "Class" "--OneIndexed" "--IgnoreFirstItem"
-"Tools/TableUpdater.py" "Tables/NightmareModules/Item/ItemTable.csv" "Definitions/Generated/ItemIDs.event" "Item" "--OneIndexed" "--IgnoreFirstItem"
-"Tools/TableUpdater.py" "Tables/NightmareModules/ChapterData/ChapterData.csv" "Definitions/Generated/ChapterIDs.event" "Chapter"
-"Tools/TableUpdater.py" "Tables/NightmareModules/WorldMap/Nodes.csv" "Definitions/Generated/WMNodeIDs.event" "Node
-"Tools/TableUpdater.py" "Tables/NightmareModules/WorldMap/Roads.csv" "Definitions/Generated/WMRoadIDs.event" "Road"
+Tools\TableUpdater.py Tables\NightmareModules\CharacterAndClass\CharacterTable.csv Definitions\Generated\CharacterIDs.event Character --OneIndexed
+Tools\TableUpdater.py Tables\NightmareModules\CharacterAndClass\ClassTable.csv Definitions\Generated\ClassIDs.event Class --OneIndexed --IgnoreFirstItem
+Tools\TableUpdater.py Tables\NightmareModules\Item\ItemTable.csv Definitions\Generated\ItemIDs.event Item --OneIndexed --IgnoreFirstItem
+Tools\TableUpdater.py Tables\NightmareModules\ChapterData\ChapterData.csv Definitions\Generated\ChapterIDs.event Chapter
+Tools\TableUpdater.py Tables\NightmareModules\WorldMap\Nodes.csv Definitions\Generated\WMNodeIDs.event Node
+Tools\TableUpdater.py Tables\NightmareModules\WorldMap\Roads.csv Definitions\Generated\WMRoadIDs.event Road

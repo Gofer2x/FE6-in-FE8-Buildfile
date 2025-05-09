9cc4caf6b4cd8d4fdca6fa6a603aab442925df69
Gofer2x <45763255+Gofer2x@users.noreply.github.com>
2025-05-09 20:58:40 +0200

    sound insert overhaul

--- SoundPriorityFix.cmd
+++
@@ -1,3 +0,0 @@
-copy myHackWithMusicBroken.gba myHackWithMusic.gba
-cd "%~dp0EventAssembler"
-ColorzCore A FE8 "-output:%~dp0myHackWithMusic.gba" "-input:%~dp0SoundPriorityFixEA.event" "--build-times"
---
+++ Tools/SoundScripts/GenerateSoundBat.py
@@ -0,0 +1,60 @@
+import sys, csv
+
+generatedBatPath = sys.argv[1]
+csvPath = sys.argv[2]
+febPath = sys.argv[3]
+fromROMPath = sys.argv[4]
+targetROMPath = sys.argv[5]
+soundPrioFixPath = sys.argv[6]
+#generatedBatPath = "_GeneratedFE6SoundInsertionAndFix.bat"
+#csvPath = "FE6toFE8Sounds.csv"
+#febPath = "Tools\\FEBuilderGBA\\FEBuilderGBA.exe"
+#fromROMPath = "FE6.gba"
+#targetROMPath = "myHackWithMusicTest.gba"
+#soundPrioFixPath = "Tools\\SoundScripts\\SoundPriorityFixEA.event"
+
+def intToHex(i):
+    j = hex(i)
+    j = j.upper()
+    j = j.replace("X", "x")
+    return str(j)
+
+def formatCmd(fromSongID, toSongID):
+    return(f"{febPath} --rom={targetROMPath} --songexchange --fromrom={fromROMPath} --target={targetROMPath} --fromsong={fromSongID} --tosong={toSongID}\n")
+
+csvData = []
+with open(csvPath, mode ='r', encoding="utf-8")as file:
+    csvFile = csv.DictReader(file)
+    for lines in csvFile:
+        csvData.append(lines)
+
+fe6ID = None
+fe8ID = None
+cmdOutput = []
+
+for item in csvData:
+    name = item["Name"]
+
+    #If the ID is "i", means iterative. Else, actual number.
+    if item["FE6 ID"] == "i":
+        fe6ID = int(fe6ID,16)
+        fe6ID += 1
+        fe6ID = intToHex(fe6ID)
+    else:
+        fe6ID = item["FE6 ID"]
+
+    if item["FE8 Target ID"] == "i":
+        fe8ID = int(fe8ID,16)
+        fe8ID += 1
+        fe8ID = intToHex(fe8ID)
+    else:
+        fe8ID = item["FE8 Target ID"]
+    
+    cmdOutput.append(formatCmd(fe6ID,fe8ID))
+
+cmdOutput.append("cd %~dp0EventAssembler\n")
+cmdOutput.append(f"ColorzCore A FE8 -output:%~dp0{targetROMPath} -input:%~dp0{soundPrioFixPath} --build-times\n")
+cmdOutput.append("cd %~dp0")
+
+with open(generatedBatPath,"w") as w:
+    w.writelines(cmdOutput)

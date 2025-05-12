0d44a495b7452804463c3a088db82cda905bd1b9
Gofer2x <45763255+Gofer2x@users.noreply.github.com>
2025-05-12 17:16:52 +0200

    parseargs for tables

--- Tools/IDsFromTables.py
+++ Tools/IDsFromTables.py
@@ -1,58 +1,58 @@
-import csv, os, time, sys
+import argparse, csv, os, time
 
 ENUMERATE = "Tools\\Enumerate\\Enumerate.py"
 
-#Used for Items, Classes...
-ignoreFirstItem = False 
+parser = argparse.ArgumentParser(description='Reads Nightmare .csv tables to automatically create EventAssembler ID definitions using Enumerate.')
+parser.add_argument('input', type=str, help='Path of the input .csv to process.')
+parser.add_argument('output', type=str, help='Path of the final output .event file with IDs.')
+parser.add_argument('valueType', type=str, help='Name of the type of object being ID\'d. Used for blank items.')
+parser.add_argument('--ignoreFirstItem', action='store_true', help='Ignores the first item in the .csv table.')
+parser.add_argument('--oneIndexed', action='store_true', help='Starts the IDs at 1 instead of 0.')
+args = parser.parse_args()
 
-#Used for Chapters, Nodes...
-oneIndexed = False
+input = args.input
+output = args.output
+valueType = args.valueType
+ignoreFirstItem = args.ignoreFirstItem
+oneIndexed = args.oneIndexed
 
-inputPath = sys.argv[1]
-outputPath = sys.argv[2]
-valueType = sys.argv[3]
-if "--IgnoreFirstItem" in sys.argv:
-    ignoreFirstItem = True
-if "--OneIndexed" in sys.argv:
-    oneIndexed = True
+# Filename without extension.
+filename = os.path.split(input)[1].replace(".csv","")
 
-filename = os.path.split(inputPath)[1].replace(".csv","")
-
-
+# Read .csv data.
 fields = []
 rows = []
-
-with open(inputPath, 'r') as csvfile:
+with open(input, 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     fields = next(csvreader)
     for row in csvreader:
         rows.append(row)
 
 values = []
+# Take leftmost row's value and use it as a name.
 for row in rows:
     values.append(row[0])
 
-
 blankCounter = 1
 
-# Item and Class tables have an invalid first item
+# Cut off first item.
 if ignoreFirstItem:
     values = values[1:]
 
-# First value as 1
+# First value as 1 or 0.
 if oneIndexed:
     values[0] = values[0]+" 1"
 else:
     values[0] = values[0]+" 0"
 for i in range(len(values)):
     
-    # Replace the empty values with "BlankVALUE"
+    # Replace the empty values with "BlankVALUE".
     if values[i] == "":
         values[i] = "Blank"+valueType+str(blankCounter)
         blankCounter += 1
     values[i] = values[i]+"\n"
 
-# File to use with Enumerator
+# Temporary file to use with Enumerator.
 tempFilePath = "Temp"+filename+".txt"
 
 print(f"Writing {tempFilePath}")
@@ -60,8 +60,8 @@ with open(tempFilePath,'w') as write:
     write.writelines(values)
 
 print(f"Running Enumerator on {tempFilePath}")
-os.system('%s %s %s' % (ENUMERATE, tempFilePath, outputPath))
+os.system('%s %s %s' % (ENUMERATE, tempFilePath, output))
 os.remove(tempFilePath)
 
 print("Exiting.")
-time.sleep(1)
+#time.sleep(1)
--- _UpdateIDsFromTables.cmd
+++ _UpdateIDsFromTables.cmd
@@ -1,6 +1,6 @@
-Tools\IDsFromTables.py Tables\NightmareModules\CharacterAndClass\CharacterTable.csv Definitions\Generated\CharacterIDs.event Character --OneIndexed
-Tools\IDsFromTables.py Tables\NightmareModules\CharacterAndClass\ClassTable.csv Definitions\Generated\ClassIDs.event Class --OneIndexed --IgnoreFirstItem
-Tools\IDsFromTables.py Tables\NightmareModules\Item\ItemTable.csv Definitions\Generated\ItemIDs.event Item --OneIndexed --IgnoreFirstItem
+Tools\IDsFromTables.py Tables\NightmareModules\CharacterAndClass\CharacterTable.csv Definitions\Generated\CharacterIDs.event Character --oneIndexed
+Tools\IDsFromTables.py Tables\NightmareModules\CharacterAndClass\ClassTable.csv Definitions\Generated\ClassIDs.event Class --oneIndexed --ignoreFirstItem
+Tools\IDsFromTables.py Tables\NightmareModules\Item\ItemTable.csv Definitions\Generated\ItemIDs.event Item --oneIndexed --ignoreFirstItem
 Tools\IDsFromTables.py Tables\NightmareModules\ChapterData\ChapterData.csv Definitions\Generated\ChapterIDs.event Chapter
 Tools\IDsFromTables.py Tables\NightmareModules\WorldMap\Nodes.csv Definitions\Generated\WMNodeIDs.event Node
 Tools\IDsFromTables.py Tables\NightmareModules\WorldMap\Roads.csv Definitions\Generated\WMRoadIDs.event Road

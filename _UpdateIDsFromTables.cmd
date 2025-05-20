d33bdd28fc7a98595eb13ad2c7e6a5f24462c2c7
Gofer2x <45763255+Gofer2x@users.noreply.github.com>
2025-05-20 12:15:00 +0200

    Update _UpdateIDsFromTables.cmd

--- _UpdateIDsFromTables.cmd
+++ _UpdateIDsFromTables.cmd
@@ -1,8 +1,8 @@
-set idsFromtables=Tools\IDsFromTables.py
+set idsFromTables=Tools\IDsFromTables.py
 set enumerate=Tools\Enumerate\Enumerate.py
-%idsFromtables% Tables\NightmareModules\CharacterAndClass\CharacterTable.csv Definitions\Generated\CharacterIDs.event %enumerate% Character --oneIndexed
-%idsFromtables% Tables\NightmareModules\CharacterAndClass\ClassTable.csv Definitions\Generated\ClassIDs.event Class %enumerate% --oneIndexed --ignoreFirstItem
-%idsFromtables% Tables\NightmareModules\Item\ItemTable.csv Definitions\Generated\ItemIDs.event Item %enumerate% --oneIndexed --ignoreFirstItem
-%idsFromtables% Tables\NightmareModules\ChapterData\ChapterData.csv Definitions\Generated\ChapterIDs.event Chapter %enumerate%
-%idsFromtables% Tables\NightmareModules\WorldMap\Nodes.csv Definitions\Generated\WMNodeIDs.event Node %enumerate%
-%idsFromtables% Tables\NightmareModules\WorldMap\Roads.csv Definitions\Generated\WMRoadIDs.event Road %enumerate%
+%idsFromTables% Tables\NightmareModules\CharacterAndClass\CharacterTable.csv Definitions\Generated\CharacterIDs.event %enumerate% Character --oneIndexed
+%idsFromTables% Tables\NightmareModules\CharacterAndClass\ClassTable.csv Definitions\Generated\ClassIDs.event Class %enumerate% --oneIndexed --ignoreFirstItem
+%idsFromTables% Tables\NightmareModules\Item\ItemTable.csv Definitions\Generated\ItemIDs.event Item %enumerate% --oneIndexed --ignoreFirstItem
+%idsFromTables% Tables\NightmareModules\ChapterData\ChapterData.csv Definitions\Generated\ChapterIDs.event Chapter %enumerate%
+%idsFromTables% Tables\NightmareModules\WorldMap\Nodes.csv Definitions\Generated\WMNodeIDs.event Node %enumerate%
+%idsFromTables% Tables\NightmareModules\WorldMap\Roads.csv Definitions\Generated\WMRoadIDs.event Road %enumerate%
